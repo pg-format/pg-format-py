2aa0e3da2aa205e321afab61ece7d578e6bc1bc3
Jakob Voss <voss@gbv.de>
2024-08-02 18:42:38 +0200

    Release 0.2.0

--- pgformat/__init__.py
+++ pgformat/__init__.py
@@ -1,5 +1,5 @@
 """Property Graph Exchange Format (PG) parser and serializer"""
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 
 from .parser import parseGraph, parseStatements
 from .serializer import serializeGraph
